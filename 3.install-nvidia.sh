33807ee9e648a6933442cfbde8df4c4af2fd2a55
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-08-23 11:41:30 -0600

    Update 3.install-nvidia.sh

--- 3.install-nvidia.sh
+++ 3.install-nvidia.sh
@@ -9,4 +9,4 @@ echo -e "\033[0;32m=====================================\033[0m"
 
 # https://forums.rockylinux.org/t/nvidia-drivers-on-rocky-linux/12366
 # install nvidia driver from nvidia's official repo
-sudo dnf module install nvidia-driver:latest-dkms
+sudo dnf module install nvidia-driver:latest-dkms -y
