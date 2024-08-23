f405174bea762e6372fb21bfd9713238ad97ee6c
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-08-23 10:49:47 -0600

    Update 4.set-desktopenvironment.sh

--- 4.set-desktopenvironment.sh
+++ 4.set-desktopenvironment.sh
@@ -9,3 +9,8 @@ echo -e "\033[0;32m=====================================\033[0m"
 
 sudo systemctl set-default graphical.target
 sudo systemctl enable sddm
+
+echo "$APP_NAME has been configured as your Desktop Environment. Press Enter to reboot into your system..."
+read
+# Reboot if you had a kernel update
+reboot
