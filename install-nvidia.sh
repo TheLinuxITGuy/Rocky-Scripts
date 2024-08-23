098f497fdd9bc1ab829cbd9363599ce721a15428
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-08-23 10:19:48 -0600

    Add files via upload

---
+++ install-nvidia.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+APP_NAME="nvidia-driver:latest-dkms"
+
+echo -e "\033[0;32m====================================="
+echo -e "\033[1;32mThe Linux IT Guy - Rocky Linux 9.4 Install NVIDIA Driver Script"
+echo -e "\033[1;32mInstalling $APP_NAME"
+echo -e "\033[0;32m=====================================\033[0m"
+
+# https://forums.rockylinux.org/t/nvidia-drivers-on-rocky-linux/12366
+# install nvidia driver from nvidia's official repo
+sudo dnf module install nvidia-driver:latest-dkms
+
+fi
