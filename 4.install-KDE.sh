45436d7fa58b6ed0477ec1f01308082d52eb68e0
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-09-13 07:56:39 -0600

    Update 4.install-KDE.sh

--- 4.install-KDE.sh
+++ 4.install-KDE.sh
@@ -9,4 +9,5 @@ echo -e "\033[0;32m=====================================\033[0m"
 
 # https://forums.rockylinux.org/t/nvidia-drivers-on-rocky-linux/12366
 # Install KDE
-sudo dnf install plasma-desktop kscreen sddm kde-gtk-config dolphin konsole kate plasma-discover firefox rocky-backgrounds sddm-breeze mpv  -y
+sudo dnf groupinstall "KDE Plasma Workspaces" -y
+sudo dnf install firefox rocky-backgrounds sddm-breeze mpv  -y
